61c5e7f9a52bebd7b6930eb5aa7d16778792a059
kevinbdx35 <k.bardoux@orange.fr>
2021-12-28 14:41:14 +0100

    add challenge-2

---
+++ challenge-2/main.js
@@ -0,0 +1,14 @@
+let myTime = 19;
+let output;
+
+if (myTime < 11) {
+  output = "Good Morning";
+} else if (myTime >= 11 && myTime <= 17) {
+  output = "Good Afternoon";
+} else if (myTime > 17 && myTime < 23) {
+  output = "Good Evening";
+} else {
+  output = "I'm sleeping";
+}
+
+console.log(output);
