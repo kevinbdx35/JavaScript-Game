3ef6dee23a28186b8fb1848ca0257dfd3d4f3212
kevinbdx35 <k.bardoux@orange.fr>
2021-12-28 14:46:58 +0100

    add course-12

---
+++ course-12/main.js
@@ -0,0 +1,13 @@
+let person = "Laurence";
+switch (person) {
+  case "John":
+    console.log(person + "is not my friend");
+    break;
+  case "Laurence":
+    console.log("found it");
+  case "Steve":
+    console.log("Hi Steve");
+    break;
+  default:
+    console.log("Nobody found");
+}
