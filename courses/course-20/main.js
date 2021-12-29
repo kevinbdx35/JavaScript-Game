23ae6793411f5d7ac1404f5a68552195ebb6d880
kevinbdx35 <k.bardoux@orange.fr>
2021-12-29 17:14:49 +0100

    add course-20

--- courses/course-20/main.js
+++ courses/course-20/main.js
@@ -1,11 +1,19 @@
 //Use the same template of index.html that before like in course-1
 
-(function (firstName) {
-  ///Block code
-  console.log("Welcome " + firstName);
-})("Laurence");
+let secretNumber = 5;
+let guess;
 
-let result = (function () {
-  return "Hello World";
-});
-console.log(result());
+guesser();
+
+function guesser() {
+  guess = prompt("Guess the number");
+  let guessNumber = Number(guess);
+  if (guessNumber === secretNumber) {
+    console.log("You got it");
+    return;
+  } else {
+    let message = guessNumber < secretNumber ? "highter" : "lower";
+    console.log("Wrong GO" + message);
+    guess();
+  }
+}
