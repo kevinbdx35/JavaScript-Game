a4933be112339777f5aa595bbdd43519945529ae
kevinbdx35 <k.bardoux@orange.fr>
2021-12-29 16:35:16 +0100

    add course-16

---
+++ courses/course-16/main.js
@@ -0,0 +1,21 @@
+let var1, var2, var3;
+var1 = var2 = var3 = 0;
+
+function message1() {
+  var1++;
+  message();
+}
+
+function message2() {
+  var2++;
+  message();
+}
+
+function message3() {
+  var3++;
+  message();
+}
+
+function message() {
+  console.log(var1 + " " + var2 + " " + var3);
+}
