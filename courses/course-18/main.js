f7c907c8e20b99dc3e0680f7505152a987ead24f
kevinbdx35 <k.bardoux@orange.fr>
2021-12-29 16:48:17 +0100

    add course-18

---
+++ courses/course-18/main.js
@@ -0,0 +1,23 @@
+//Use the same template of index.html that before like in course-1
+
+/*
+//Function declatation
+function test(num){
+  return num;
+}
+
+//Function expression
+Const test = function(num){
+  return num;
+}
+*/
+
+const test1 = function (num) {
+  return num + 5;
+};
+console.log(test1(5));
+console.log(test2(5));
+
+function test2(num) {
+  return num + 5;
+}
