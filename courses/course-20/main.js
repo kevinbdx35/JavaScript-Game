7f567465db4afb7bee7e3e6af56ca7910c91267f
kevinbdx35 <k.bardoux@orange.fr>
2021-12-29 16:59:43 +0100

    add course-20

---
+++ courses/course-20/main.js
@@ -0,0 +1,19 @@
+//Use the same template of index.html that before like in course-1
+
+let secretNumber = 5;
+let guess;
+
+guesser();
+
+function guesser() {
+  guess = prompt("Guess the number");
+  let guessNumber = Number(guess);
+  if (guessNumber === secretNumber) {
+    console.log("You got it");
+    return;
+  } else {
+    let message = guessNumber < secretNumber ? "highter" : "lower";
+    console.log("Wrong GO" + message);
+    guess();
+  }
+}
