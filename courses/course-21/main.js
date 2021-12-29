ae266f52b208bf51b0241b0c9e4511b179563dbc
kevinbdx35 <k.bardoux@orange.fr>
2021-12-29 17:16:01 +0100

    add course-21

---
+++ courses/course-21/main.js
@@ -0,0 +1,11 @@
+//Use the same template of index.html that before like in course-1
+
+(function (firstName) {
+  ///Block code
+  console.log("Welcome " + firstName);
+})("Laurence");
+
+let result = (function () {
+  return "Hello World";
+});
+console.log(result());
