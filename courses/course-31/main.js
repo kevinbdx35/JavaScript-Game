941ed09ecc6530052fab3aa2fc04be69f622e975
kevinbdx35 <k.bardoux@orange.fr>
2021-12-30 17:03:07 +0100

    add challenge - 31

---
+++ courses/course-31/main.js
@@ -0,0 +1,24 @@
+//Use the same template of index.html that before like in course-1
+
+const obj = { a: 1, b: 2, c: 3 };
+
+console.log(obj);
+
+for (let prop in obj) {
+  console.log(prop, obj[prop]);
+}
+
+const arr = ["a", "b", "c"];
+
+for (let w = 0; w < arr.length; w++) {
+  console.log(w, arr[w]);
+}
+
+for (el in arr) {
+  console.log(el, arr[el]);
+}
+
+arr.forEach(function (el, index, array) {
+  console.log(array);
+  console.log(index, el);
+});
