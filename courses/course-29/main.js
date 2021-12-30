ce62f7df9ceb3b8a639a91ab461ac3ebb0a0a098
kevinbdx35 <k.bardoux@orange.fr>
2021-12-30 16:38:39 +0100

    add course-29

---
+++ courses/course-29/main.js
@@ -0,0 +1,11 @@
+//Use the same template of index.html that before like in course-1
+
+const myArray = ["a", "Hello", 4, 8, 2, "World", "JavaScript", "Course", 99, 1];
+const myArray2 = [5, 341, 6324, 7, 432, 12, 8, 130, 44];
+
+let result = myArray2.filter(function (num) {
+  console.log(num);
+  return numArray > 75;
+});
+
+console.log(result);
