a85b26327c53d11190a331fc1233f8cc80732f30
kevinbdx35 <k.bardoux@orange.fr>
2021-12-30 15:44:24 +0100

    add course-23

---
+++ courses/course-23/main.js
@@ -0,0 +1,17 @@
+//Use the same template of index.html that before like in course-1
+
+const car = {
+  color: "red",
+  year: 2010,
+  make: "Corvette",
+  brand: "Chevrolet",
+  price: 50000,
+};
+
+let a = 1;
+const test = {
+  a1:"test1";
+  a2:"test2"
+}
+
+console.log(test);
