b02692c58a5b82c230e5aa3944fed75d9e0b39c7
kevinbdx35 <k.bardoux@orange.fr>
2021-12-30 16:45:39 +0100

    add course-30

---
+++ courses/course-30/main.js
@@ -0,0 +1,19 @@
+//Use the same template of index.html that before like in course-1
+
+for (let i = 0; i < 5; i++) {
+  console.log("For Loop" + i);
+}
+
+let x = 0;
+while (x < 5) {
+  //x++;
+  console.log("While Loop" + x);
+  x++;
+}
+
+let i = 10;
+do {
+  //i++;
+  console.log("Do While" + i);
+  i++;
+} while (i < 5);
