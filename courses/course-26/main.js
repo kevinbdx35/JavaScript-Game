3575c6b72f68c8014223159adaeab6660252f63b
kevinbdx35 <k.bardoux@orange.fr>
2021-12-30 16:03:45 +0100

    add course-26

---
+++ courses/course-26/main.js
@@ -0,0 +1,15 @@
+//Use the same template of index.html that before like in course-1
+
+const theList = [
+  "Laurence",
+  "Svekis",
+  true,
+  35,
+  null,
+  undefined,
+  {
+    test: "one",
+    score: 55,
+  },
+  ["one", "two"],
+];
