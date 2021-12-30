ef46ca9bf2fc765088070c8d31c394bb26cc7028
kevinbdx35 <k.bardoux@orange.fr>
2021-12-30 17:08:41 +0100

    add course - 32

---
+++ courses/course-32/main.js
@@ -0,0 +1,10 @@
+//Use the same template of index.html that before like in course-1
+
+const numArray = [4, 543, 12, 34, 234, 54, 2, 3, 443, 434334, 3255];
+
+let mapArray = numArray.map(function (x) {
+  console.log(x);
+  return x * 50;
+});
+
+console.log(mapArray);
