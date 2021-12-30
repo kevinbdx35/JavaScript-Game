d5b576beffe6290fa763a27e665642c3060eebf9
kevinbdx35 <k.bardoux@orange.fr>
2021-12-30 15:40:15 +0100

    add course-22

---
+++ courses/course-22/main.js
@@ -0,0 +1,11 @@
+//Use the same template of index.html that before like in course-1
+
+const test = function (x) {
+  return x * 10;
+};
+
+console.log(test(90));
+
+const test2 = (x) => x * 10;
+
+console.log(test2(90));
