9e30a64efe8efe780a9a602bf4ee2b0484ba6a14
kevinbdx35 <k.bardoux@orange.fr>
2022-01-03 17:18:14 +0100

    add multiple element selection

---
+++ DOM-courses/2-multiple-element-selection/main.js
@@ -0,0 +1,20 @@
+//Use the same template of index.html that before, to implement this code
+
+let eleList = document.getElementsByClassName("first");
+eleList = document.getElementsByTagName("span");
+eleList = document.querySelectorAll(".first");
+for (let i = 0; i < eleList.length; i++) {
+  let el = eleList[i];
+  console.log(el);
+  eleList[i].textContent = (i +1) + " updated";
+}
+
+eleList.forEach(function(el,index){
+    console.log(index);
+    el.textContent = `${index}: updated`;
+    el.style.backgroundColor = 'red';
+})///Only for nodelist
+    
+
+
+console.log(eleList);
