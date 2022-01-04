9ade6fc9c6db9a5296e71930501a70102ca042be
kevinbdx35 <k.bardoux@orange.fr>
2022-01-04 15:41:45 +0100

    add Element Style Updates

---
+++ DOM-courses/6-element-style-updates/main.js
@@ -0,0 +1,10 @@
+//Use the same template of index.html that before, to implement this code
+
+const h1 = document.querySelector('h1');
+
+h1.style.backgroundColor = "red";
+h1.style.color = "yellow";
+h1.style.display = "none";
+h1.style.display = "block";
+h1.style.border = "5px dotted purple";
+h1.style.padding = "25px";
